bb3e6fe7d28785892f875b10652090d355d62c33
xkai003 <xkai03@outlook.com>
2026-03-29 23:41:58 +0800

    优化首页及添加毕业设计论文

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,28 @@
 <template>
-  <div>
+  <div class="box">
     <Navigation></Navigation>
+    <!-- 条件判断：不在 noNavRoutes 里才显示导航 -->
+    <!-- <Navigation v-if="!noNavRoutes.includes($route.path)" /> -->
+
+    <!-- 路由页面出口 -->
+    <!-- <router-view /> -->
   </div>
 </template>
 
-<script setup>
-import Navigation from './components/Navigation.vue';
+<script>
+import Navigation from './components/Navigation.vue'
+export default {
+  components: {
+    Navigation
+  },
+  computed: {
+    // noNavRoutes() {
+    //   // 在这里写 不需要导航栏 的路由
+    //   return ['/']
+    // }
+  }
+}
 </script>
 
-<style>
-
+<style scoped>
 </style>
